04de90d3f5f966dea6e65c14a8c369a1627a64c3
Dillon Barker <dor.barker@gmail.com>
2017-08-31 16:59:55 -0600

    Revamp

--- crowbar/test/simulate_recovery.py
+++ crowbar/test/simulate_recovery.py
@@ -9,11 +9,13 @@ import crowbar  # main script
 
 # regular imports
 import argparse
+import json
 import random
+from concurrent.futures import ProcessPoolExecutor
 from functools import partial
 from itertools import chain
 from pathlib import Path
-from typing import Dict, Tuple
+from typing import Dict, Tuple, Union
 # 3rd Party imports
 import pandas as pd
 import numpy as np
@@ -88,6 +90,8 @@ def introduce_random_errors(trunc_prob: float, miss_prob: float,
 
     error_calls = calls.copy()
 
+    truncations = {}
+
     for strain, row in error_calls.iterrows():
 
         to_truncate = [random.random() < trunc_prob for _ in row]
@@ -96,8 +100,9 @@ def introduce_random_errors(trunc_prob: float, miss_prob: float,
         row[to_truncate] = [-1 for _ in row]
         row[to_vanish] = [0 for _ in row]
 
-        truncations = {gene: truncate(strain, gene, jsondir)
-                       for gene in row[to_truncate].index}
+        truncations[strain] = {gene: truncate(strain, gene, jsondir)
+                               for gene in row[to_truncate].index}
+
 
     return error_calls, truncations
 
@@ -114,81 +119,86 @@ def truncate(strain: str, gene: str, jsondir: Path, tempdir: Path):
 
     return halves[side]
 
+def create_dummy_jsons(truncations: Dict[str, Dict[str, str]], tempdir: Path):
 
-def recover_alleles(truncations: Dict[str, str], distances: np.matrix,
-                    calls: pd.DataFrame, genes: Path):
+    for strain in truncations:
 
-    gene_names = calls.columns
+        genes = {gene: {'Amplicon': truncations[strain][gene]}
+                 for gene in truncations[strain]}
 
-    results = {}
+        data = {'Results': [{'TestResults': {'dummy': genes}}]}
 
-    for strain, row in calls.iterrows():
-        results[strain] = {}
+        json_path = (jsondir / strain).with_suffix('.json')
 
-        # treat truncated and missing differently,
-        # as missing loci have no fragment to use
-        truncated_genes = row[row == -1].index
-        missing_genes = row[row == 0].index
-
-        for gene in chain(truncated_genes, missing_genes):
-
-            ### Nearest Neighbour
-            closest_alleles = crowbar.closest_relative_allele(strain, gene,
-                                                              calls, distances)
+        with json_path.open('w') as out:
+            json.dump(data, out)
 
 
-            ### Linkage disequilibrium
-            triplets = count_triplets(gene, calls)
-            left_col = gene_names[gene_names.index(gene) - 1]
-            right_col = gene_names[gene_names.index(gene) + 1]
+def recover_simulated(strain: str, gene: str, calls: pd.DataFrame,
+                      distances: np.matrix, genes: Path, jsondir: Path,
+                      replicates: int, seed: int) -> Union[int, str]:
 
-            left, right = calls[[left_col, right_col]].loc[strain]
-            linked = triplets[left][right]
-
+    probs = crowbar.recover_allele(strain, gene, calls, distances, genes,
+                                   jsondir, replicates, seed)
 
 
-            ### Fragment matching
-            try:
-                matching_fragments = crowbar.fragment_match(gene,
-                                                            truncations[gene],
-                                                            genes)
+    most_likely_allele = max(probs, key=lambda x: probs[x])
 
-                partial_matches_only = partial(crowbar.exclude_matches,
-                                               include=matching_fragments)
-
-                closest_alleles = partial_matches_only(closest_alleles)
-
-                linked = {centre: linked[centre]
-                          for centre in partial_matches_only(linked.keys())}
-            except KeyError:
-
-                matching_fragments = set()
-
-            results[strain][gene] = {'linkage': linked,
-                                     'closest': closest_alleles,
-                                     'fragments': matching_fragments}
-
-    return results
+    return most_likely_allele
 
 
-def recover(truncation_probability: float, missing_probability: float,
-            seed: int, calls: pd.DataFrame, jsondir: Path, genes: Path,
-            cores: int):
+def simulate_recovery(truncation_probability: float, missing_probability: float,
+                      calls: pd.DataFrame, jsondir: Path, genes: Path,
+                      seed: int, replicates: int, cores: int):
 
-    distances = crowbar.dist_gene(calls, cores)
 
     error_calls, truncations = introduce_random_errors(truncation_probability,
                                                        missing_probability,
                                                        calls, jsondir, seed)
 
-    recovered_alleles = recover_alleles(truncations, distances,
-                                        error_calls, genes)
 
+    error_calls, truncations = introduce_random_errors(truncation_probability,
+    create_dummy_jsons(truncations, tempdir)
+
+    distances = crowbar.hamming_distance_matrix(None, error_calls, cores)
+
+    recover = partial(recover_simulated, calls=error_calls,
+                      distances=distances, genes=genes,jsondir=tempdir,
+                      seed=seed, replicates=replicates)
+
+    result_futures = {}
+
+    with ProcessPoolExecutor(max_workers=cores) as ppe:
+
+        for strain in error_calls.index:
+
+            for gene in error_calls.columns:
+
+                if error_calls.loc[strain, gene] > 0:
+                    continue
+
+                recovered = ppe.submit(recover, strain, gene)
+
+                result = {'recovered': recovered,
+                          'original': calls.loc[strain, gene]}
+
+                try:
+                    result_futures[strain][gene] = result
+
+                except KeyError:
+                    result_futures[strain] = {}
+                    result_futures[strain][gene] = result
+
+    return result_futures
 
 def main():
 
     args = arguments()
 
+    assert args.tempdir != args.jsondir, 'tempdir cannot equal jsondir'
+
+    recover(args.trunc_prob, args.miss_prob, args.distances, args.calls,
+            args.jsons, args.genes, args.seed, args.replicates, args.cores)
 
 if __name__ == '__main__':
     main()
